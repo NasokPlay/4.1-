eb9745c71129b68d427b9e1f73026079929c8fd5
NasokPlay <danya.semenoff2018@yandex.ru>
2021-12-10 12:03:30 +0700

    master

---
+++ 4.1 черт побери/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4._1_черт_побери
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int rez = 0;
+            char c;
+
+            Console.WriteLine("Введите а и b, или выберите операцию: +, -, *, /");
+            c = Convert.ToChar(Console.ReadLine());
+            switch (c)
+            {
+
+            }
+            Console.Write(rez);
+            Console.ReadKey();
+        }
+    }
+}
